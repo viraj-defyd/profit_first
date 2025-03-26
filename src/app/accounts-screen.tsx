989737ac8b7271ad702c665bcc89eda5395631e8
viraj <viraj@defyd.com>
2025-03-26 18:01:37 +0530

    merging with main

--- src/app/accounts-screen.tsx
+++ src/app/accounts-screen.tsx
@@ -1,16 +1,30 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
+import {
+  TextInput,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
 import React from "react";
 import AccountList from "../components/account-list";
+import Entypo from "@expo/vector-icons/Entypo";
 const AccountsScreen: React.FC = () => {
   return (
-    <View style={{ gap: 5, padding: 5 }}>
-      <View style={styles.header}>
-        <Text>Name</Text>
-        <Text>CAP</Text>
-        <Text>Tap</Text>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={{ gap: 5, padding: 5 }}>
+        <View style={styles.header}>
+          <Text>Name</Text>
+          <Text>CAP</Text>
+          <Text>Tap</Text>
+        </View>
+        <AccountList />
+        <View style={styles.inputRow}>
+          <TextInput placeholder="Name" style={styles.input} />
+          <TextInput placeholder="CAP %" style={styles.input} />
+          <TextInput placeholder="TAP %" style={styles.input} />
+          <Entypo name="check" size={24} color="black" />
+        </View>
+        <Button title="Add Account" />
       </View>
-      <AccountList />
-    </View>
+    </GestureHandlerRootView>
   );
 };
 
@@ -22,4 +36,11 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     padding: 10,
   },
+  inputRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 10,
+    backgroundColor: "white",
+  },
+  input: {},
 });
